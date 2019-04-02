cae418c5e9cca57adf57ed1eb71addc1c964bb0f
Shamyakov Kirya <Shamyakov.kirya@gmail.com>
2019-04-03 00:35:42 +0300

    в душе не ебу что я тут сделал

---
+++ Shop.WebApplication/Shop.Shared/Entities/UserPrincipal.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Security.Principal;
+
+namespace Shop.Shared.Entities
+{
+    public class UserPrincipal :  IPrincipal
+    {
+        public UserPrincipal(string userName)
+        {
+            this.Identity = new GenericIdentity(userName);
+        }
+
+        public IIdentity Identity { get; private set; }
+    } //доделать!
+}
---
+++ Shop.WebApplication/Shop.Web/Attributes/AdminAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Shop.Shared.Entities;
+using Shop.Shared.Entities.Enums;
+
+namespace Shop.Web.Attributes
+{
+    public class AdminAttribute : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            var user = HttpContext.Current.User;
+            if(user is User)
+            {
+                return user.IsInRole(RoleType.Administrator.ToString());
+            }
+            return false;
+        }
+    }
+}
